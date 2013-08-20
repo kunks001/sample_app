78bfea7f24cad4b8add49dd571ff19ded868eaa4
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-08-20 16:13:44 +0100

    updating-users 4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -34,10 +35,16 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 end
