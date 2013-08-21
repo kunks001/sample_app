0592b242eb175aaddd87d758083a6f42931d87e0
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-08-21 09:52:15 +0100

    Adding authentication to micropost controller actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -51,20 +51,20 @@ describe "AuthenticationPages" do
       let(:user) { FactoryGirl.create(:user) }
 
     describe "when attempting to visit a protected page" do
-        before do
-          visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
-        end
-
-        describe "after signing in" do
-
-          it "should render the desired protected page" do
-            expect(page).to have_title('Edit user')
-          end
-        end
+      before do
+        visit edit_user_path(user)
+        fill_in "Email",    with: user.email
+        fill_in "Password", with: user.password
+        click_button "Sign in"
       end
+
+      describe "after signing in" do
+
+        it "should render the desired protected page" do
+        expect(page).to have_title('Edit user')
+      end
+    end
+  end
 
       describe "in the Users controller" do
 
@@ -84,6 +84,19 @@ describe "AuthenticationPages" do
         end
       end
     end
+
+    describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
 
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
