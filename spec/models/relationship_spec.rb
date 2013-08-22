5d931601875186109945d1e43ce24008b6dcdc6c
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-08-22 09:34:21 +0100

    Established relationship between follower and followed users

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -9,4 +9,14 @@ describe Relationship do
   subject { relationship }
 
   it { should be_valid }
+
+  describe "when followed id is not present" do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when follower id is not present" do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
+  end
 end
